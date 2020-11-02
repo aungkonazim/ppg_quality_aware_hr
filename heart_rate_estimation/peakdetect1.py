2c6becc52a124bb56730d47a6d10a73f079918c6
Azim <aungkonazim@gmail.com>
2020-11-01 18:17:42 -0600

    changed name and added heart rate estimation from ppg using spectral tracking

---
+++ heart_rate_estimation/peakdetect1.py
@@ -0,0 +1,104 @@
+
+### Peak Detection codes ##
+import numpy as np
+def _datacheck_peakdetect(x_axis, y_axis):
+    """
+    check data for peak detection
+
+    :param x_axis: time
+    :param y_axis: values
+    :return: same as input data
+    """
+    if x_axis is None:
+        x_axis = range(len(y_axis))
+
+    if len(y_axis) != len(x_axis):
+        raise ValueError("Input vectors y_axis and x_axis must have same length")
+
+    #needs to be a numpy array
+    y_axis = np.array(y_axis)
+    x_axis = np.array(x_axis)
+    return x_axis, y_axis
+
+
+def peakdetect(y_axis, x_axis = None, lookahead = 200, delta=0):
+    import numpy as np
+    """
+
+    :param y_axis: values
+    :param x_axis: time
+    :param lookahead: steps ahead to look for
+    :param delta:
+    :return: peak locations
+
+    """
+    max_peaks = []
+    min_peaks = []
+    dump = []   #Used to pop the first hit
+
+    # check input data
+    x_axis, y_axis = _datacheck_peakdetect(x_axis, y_axis)
+    # store data length for later use
+    length = len(y_axis)
+
+
+    #perform some checks
+    if lookahead < 1:
+        raise ValueError("Lookahead must be '1' or above in value")
+    if not (np.isscalar(delta) and delta >= 0):
+        raise ValueError("delta must be a positive number")
+
+    #maxima and minima candidates are temporarily stored in
+    #mx and mn respectively
+    mn, mx = np.Inf, -np.Inf
+
+    #Only detect peak if there is 'lookahead' amount of points after it
+    for index, (x, y) in enumerate(zip(x_axis[:-lookahead],
+                                       y_axis[:-lookahead])):
+        if y > mx:
+            mx = y
+            mxpos = x
+        if y < mn:
+            mn = y
+            mnpos = x
+
+        ####look for max####
+        if y < mx-delta and mx != np.Inf:
+            #Maxima peak candidate found
+            #look ahead in signal to ensure that this is a peak and not jitter
+            if y_axis[index:index+lookahead].max() < mx:
+                max_peaks.append([mxpos, mx])
+                dump.append(True)
+                #set algorithm to only find minima now
+                mx = np.Inf
+                mn = np.Inf
+                if index+lookahead >= length:
+                    #end is within lookahead no more peaks can be found
+                    break
+                continue
+
+        ####look for min####
+        if y > mn+delta and mn != -np.Inf:
+            #Minima peak candidate found
+            #look ahead in signal to ensure that this is a peak and not jitter
+            if y_axis[index:index+lookahead].min() > mn:
+                min_peaks.append([mnpos, mn])
+                dump.append(False)
+                #set algorithm to only find maxima now
+                mn = -np.Inf
+                mx = -np.Inf
+                if index+lookahead >= length:
+                    #end is within lookahead no more peaks can be found
+                    break
+
+    #Remove the false hit on the first value of the y_axis
+    try:
+        if dump[0]:
+            max_peaks.pop(0)
+        else:
+            min_peaks.pop(0)
+        del dump
+    except IndexError:
+        pass
+
+    return [max_peaks, min_peaks]
